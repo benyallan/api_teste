63b7311d2a31b49a2bea473017dd00ff921b9e48
Beny Allan <benyallan@yahoo.com.br>
2023-05-06 06:18:56 -0300

    Fix: Company hasMany Users

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Crypt;
 
@@ -24,5 +25,10 @@ class Company extends Model
             get: fn (string $value) => Crypt::decryptString($value),
             set: fn (string $value) => Crypt::encryptString($value),
         );
+    }
+
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class);
     }
 }
